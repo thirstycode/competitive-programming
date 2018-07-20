59fc5ade87e7ef32095cf023f8dd76c0d0ec8353
Pratik Kinage <pratikkinage@gmail.com>
2018-07-20 15:50:18 +0530

    Update sol.py

--- Bytelandian gold coins/sol.py
+++ Bytelandian gold coins/sol.py
@@ -1,1 +1,19 @@
+import sys
 
+sol_list = {} #to reduce the recursive computation #really nice trick
+
+def sol(num):
+	if num <=	11:
+		return num
+	if num in sol_list.keys():
+		return sol_list[num]
+	else:
+		sol_list[num] = sol(int(num/2)) + sol(int(num/3)) + sol(int(num/4))
+		return sol_list[num]
+
+try:
+	while True:
+	    i = int(input())
+	    print(sol(i))
+except :
+    sys.exit(0)
